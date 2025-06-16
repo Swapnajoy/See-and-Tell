fdef2af04cdb4dcf38452a41ddd410894aeadfd6
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-06-16 20:22:29 +0200

    Added Wiki search and extract

--- scripts/kb_builder.py
+++ scripts/kb_builder.py
@@ -1,23 +1,51 @@
 import os
-import sys
 import json
 import spacy
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import wikipediaapi
+from tqdm import tqdm
 
 with open('data\captions\mini_coco.json', 'r', encoding='utf-8') as f:
     mini_coco = json.load(f)
 
 nlp = spacy.load("en_core_web_trf")
+wiki = wikipediaapi.Wikipedia(
+    language='en',
+    user_agent='SeeAndTellBot/1.0 (sahaswapnajoy56@gmail.com)'
+)
 
 nouns = set()
-for item in mini_coco:
+for item in tqdm(mini_coco, desc='Processing mini_coco'):
     caption = item['caption']
 
     doc = nlp(caption)
     for np in doc.noun_chunks:
-        nouns.add(np.text.split()[-1])
+        nouns.add(np.text.split()[-1].lower().strip())
         if len(nouns) == 200:
             break
 
-print("Nouns extracted")
+    if len(nouns) == 200:
+            break
+    
+print("Nouns extracted. Generating Knowledge-Base")
+
+
+entries = []
+for noun in tqdm(nouns, desc='Wiki Search'):
+    page = wiki.page(noun)
+
+    if page.exists():
+        content = page.summary[:1000]
+        entry = {
+            "title": noun,
+            "content": content,
+        }
+        entries.append(entry)
+
+kb_path = 'data/knowledge_base/wiki_entries.jsonl'
+os.makedirs(os.path.dirname(kb_path), exist_ok=True)
+
+with open(kb_path, 'w', encoding='utf-8') as g:
+    for entry in entries:
+        g.write(json.dumps(entry) + '\n')
+
+print(f"Knowledge-base generated at: {kb_path}")
