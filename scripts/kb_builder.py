3182f2d994af0f2357f32485567da2fd769b8cee
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-06-16 19:50:11 +0200

    added noun extraction using spacy

--- scripts/kb_builder.py
+++ scripts/kb_builder.py
@@ -8,10 +8,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 with open('data\captions\mini_coco.json', 'r', encoding='utf-8') as f:
     mini_coco = json.load(f)
 
-count = 0
+nlp = spacy.load("en_core_web_trf")
+
+nouns = set()
 for item in mini_coco:
-    count += 1
     caption = item['caption']
-    print(caption)
-    if count == 5:
-        break
+
+    doc = nlp(caption)
+    for np in doc.noun_chunks:
+        nouns.add(np.text.split()[-1])
+        if len(nouns) == 200:
+            break
+
+print("Nouns extracted")
