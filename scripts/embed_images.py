f25744df854536c6e91e031ad9077ddb9c5bf9fb
Swapnajoy Saha <swapnajoy.saha@rwth-aachen.de>
2025-06-18 11:53:10 +0200

    Updated embed_images to save a jsonl file

--- scripts/embed_images.py
+++ scripts/embed_images.py
@@ -3,6 +3,7 @@ import sys
 
 sys.path.append((os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))))
 
+import json
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -16,10 +17,16 @@ device = model.device
 
 files = os.listdir(image_folder)
 
-results = []
+entries = []
 for file_name in tqdm(files, desc='Processing Images'):
     if file_name.endswith('.jpg'):
         file_path = os.path.join(image_folder, file_name)
-        results.append(model.encode_from_path(file_path))
+        entry = {
+            'image_id': file_name,
+            'embedding': model.encode_from_path(file_path)
+        }
+        entries.append(entry)
 
-print(len(results))
+with open('data/image_embeddings.jsonl', 'w', encoding='utf-8') as f:
+    for entry in entries:
+        f.write(json.dumps(entry) + '\n')
