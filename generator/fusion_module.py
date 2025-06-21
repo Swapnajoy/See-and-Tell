ae746ac223069a707cbc3857ad47ca3e98ced0a0
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-21 09:44:46 +0000

    Updated var names in fusion_module

--- generator/fusion_module.py
+++ generator/fusion_module.py
@@ -1,15 +1,14 @@
 import torch
-import numpy as np
 
 class FusionModule:
-    def __init__(self, image_dim=768, text_dim=384, k=3):
-        self.image_dim = image_dim
-        self.text_dim = text_dim
+    def __init__(self, query_dim=768, ret_text_dim=384, k=3):
+        self.query_dim = query_dim
+        self.ret_text_dim = ret_text_dim
         self.k = k
 
-    def __call__(self, image_vec: torch.Tensor, retrieved_vecs: torch.Tensor) -> torch.Tensor:
-        assert image_vec.shape[0] == self.image_dim
-        assert retrieved_vecs.shape == (self.k, self.text_dim)
+    def __call__(self, query_vec: torch.Tensor, retrieved_vecs: torch.Tensor) -> torch.Tensor:
+        assert query_vec.shape[0] == self.query_dim
+        assert retrieved_vecs.shape == (self.k, self.ret_text_dim)
 
-        retrieved_vecs = retrieved_vecs.reshape(self.k*self.text_dim)
-        return torch.cat((image_vec, retrieved_vecs), dim=0)
+        retrieved_vecs = retrieved_vecs.reshape(self.k*self.ret_text_dim)
+        return torch.cat((query_vec, retrieved_vecs), dim=0)
