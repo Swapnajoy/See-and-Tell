adf69d93bd49a181de86042b9f330d7dcad014e1
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-22 13:58:24 +0000

    Modified call to handle only image_path

--- encoder/image_encoder.py
+++ encoder/image_encoder.py
@@ -15,11 +15,8 @@ class ImageEncoder:
                 transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
         ])
 
-    def __call__(self, image) -> torch.Tensor:
+    def __call__(self, image_path) -> torch.Tensor:
+        image = Image.open(image_path).convert('RGB')
         out = self.tf(image).unsqueeze(0).to(self.device)
         cls_token_emb = self.transformer(out).last_hidden_state[:, 0, :]
         return cls_token_emb.squeeze(0)
-    
-    def encode_from_path(self, path) -> torch.Tensor:
-        image = Image.open(path).convert('RGB')
-        return self.__call__(image)
