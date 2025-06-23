7b8d4c07ee31d8327b02d52b6fc5ffa213dd8937
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-23 17:38:20 +0000

    Experimental training script

--- train.py
+++ train.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 from tqdm import tqdm
 
 from datasets.triplet_dataset import TripletDataset
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from vlmrag.vlmrag_model import VLMRAG
 
-learning_rate = 0.003
-batch_size = 16
+learning_rate = 0.005
+batch_size = 1
 epochs = 30
 log_freq = 2
 
@@ -15,8 +15,9 @@ dataset = TripletDataset()
 
 split_idx = int(0.9*len(dataset))
 
-train_dataset = dataset[:split_idx]
-test_dataset = dataset[split_idx:]
+train_size = int(0.9 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
@@ -24,18 +25,23 @@ test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 model = VLMRAG(mode='train').to(device)
+model.unfreeze_projection_params
 
 steps_per_batch = len(train_loader)
 max_steps = epochs*steps_per_batch
 
-optimizer = torch.optim.AdamW(model.parameters(), weight_decay=1e-6)
+optimizer = torch.optim.AdamW(
+    list(model.retriever.project.parameters()) + list(model.decoder.projection.parameters()),
+    lr=learning_rate,
+    weight_decay=1e-6
+)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=max_steps)
 
 for epoch in range(epochs):
     running_loss = 0
-    for item in tqdm(train_loader, desc='Epoch {epoch}: '):
-        image_path = item['image_path']
-        query = item['query']
+    for item in tqdm(train_loader, desc=f'Epoch {epoch}: '):
+        image_path = item['image_path'][0]
+        query = item['query'][0]
         target_ids = item['target_ids'].to(device)
         target_mask = item['target_mask'].to(device)
 
@@ -51,4 +57,4 @@ for epoch in range(epochs):
     
     print(f"Epoch {epoch}, Average Loss: {running_loss/steps_per_batch:.4f}")
 
-print("Trainign Completed.")
+print("Training Completed.")
