aa320432853a8baad8a0f14ccafbc7da9f91e4d7
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-25 20:20:15 +0000

    updated train.py to incorporate config

--- train.py
+++ train.py
@@ -5,24 +5,30 @@ from tqdm import tqdm
 from datasets.triplet_dataset import TripletDataset
 from torch.utils.data import DataLoader, random_split
 from vlmrag.vlmrag_model import VLMRAG
+from utils.config import load_config
 
-learning_rate = 0.0003
-batch_size = 32
-epochs = 50
-log_and_eval_freq = epochs//epochs
+cfg = load_config()
+
+learning_rate = cfg['lr']
+batch_size = cfg['batch_size']
+epochs = cfg['epochs']
+weight_decay = cfg['weight_decay']
+log_freq = cfg['log_frequency']
+eval_freq = cfg['eval_frequency']
+train_test_split = cfg['train_test_split']
 
 dataset = TripletDataset()
 
-split_idx = int(0.9*len(dataset))
+split_idx = int(train_test_split*len(dataset))
 
-train_size = int(0.9 * len(dataset))
+train_size = split_idx
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = cfg['device']
 
 model = VLMRAG(mode='train').to(device)
 
@@ -56,9 +62,11 @@ for epoch in range(epochs):
         running_loss += loss.item()
     
     training_loss = running_loss/steps_per_batch
+    print(f"Epoch {epoch+1}: Training Loss: {training_loss:.4f}")
 
-    if (epoch+1)%log_and_eval_freq == 0:
+    if (epoch+1)%eval_freq == 0:
         model.eval()
+        validation_loss = None
         with torch.no_grad():
             running_loss = 0
             for item in test_loader:
@@ -71,7 +79,9 @@ for epoch in range(epochs):
                 running_loss += loss.item()
 
             validation_loss = running_loss/steps_per_batch
+            print(f"Epoch {epoch+1}: Validation Loss: {validation_loss:.4f}")
         
-        print(f"Epoch {epoch+1}: Training Loss: {training_loss:.4f} Validation Loss: {validation_loss:.4f}")
+    if (epoch+1)%log_freq == 0:
+        log_line = f"Epoch {epoch+1}: Training Loss: {training_loss:.4f} Validation Loss: {validation_loss:.4f}"
 
 print("Training Completed.")
