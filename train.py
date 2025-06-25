ce45e50aa324e4039cc32332331bcb08403918d3
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-06-25 20:41:54 +0000

    Added logging in training script

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import os
+import yaml
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -8,6 +10,22 @@ from vlmrag.vlmrag_model import VLMRAG
 from utils.config import load_config
 
 cfg = load_config()
+
+from datetime import datetime
+import os
+import yaml
+
+def create_exp_folder(config, base_dir="train_exp"):
+    dt_string = datetime.now().strftime("%Y%m%d-%H%M%S")
+    folder_name = f"run_epoch{config['epochs']}_lr{config['lr']}_bs{config['batch_size']}_time{dt_string}"
+    exp_path = os.path.join(base_dir, folder_name)
+    os.makedirs(exp_path, exist_ok=True)
+
+    with open(os.path.join(exp_path, 'config.yaml'), 'w') as f:
+        yaml.dump(config, f, default_flow_style=False, sort_keys=False)
+
+    log_path = os.path.join(exp_path, 'log.txt')
+    return exp_path, log_path
 
 learning_rate = cfg['lr']
 batch_size = cfg['batch_size']
@@ -42,6 +60,8 @@ optimizer = torch.optim.AdamW(
 )
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=max_steps)
 
+exp_path, log_path = create_exp_folder(config=cfg)
+
 for epoch in range(epochs):
     model.train()
     running_loss = 0
@@ -69,7 +89,7 @@ for epoch in range(epochs):
         validation_loss = None
         with torch.no_grad():
             running_loss = 0
-            for item in test_loader:
+            for item in tqdm(test_loader, desc=f"Eval {epoch+1}"):
                 image_path = item['image_path']
                 query = item['query']
                 target_ids = item['target_ids'].to(device)
@@ -82,6 +102,11 @@ for epoch in range(epochs):
             print(f"Epoch {epoch+1}: Validation Loss: {validation_loss:.4f}")
         
     if (epoch+1)%log_freq == 0:
-        log_line = f"Epoch {epoch+1}: Training Loss: {training_loss:.4f} Validation Loss: {validation_loss:.4f}"
+        log_line = f"Epoch {epoch+1} | Training Loss: {training_loss:.4f} | Validation Loss: {validation_loss:.4f}"
+        ckpt_path = os.path.join(exp_path, f'epoch_{epoch+1}.pt')
 
+        with open(log_path, 'a', encoding='utf-8') as f:
+            f.write(log_line)
+        
+        torch.save(model.state_dict(), ckpt_path)
 print("Training Completed.")
