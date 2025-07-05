c81fbf2fd125eb36274fa981a32214690ee04024
Swapnajoy <swapnajoy.saha@rwth-aachen.de>
2025-07-05 16:11:48 +0000

    scaled fusion implemented

--- decoder/fusion_module.py
+++ decoder/fusion_module.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 class FusionModule:
     def __init__(self, query_dim=1536, ret_text_dim=384, k=3):
@@ -6,13 +7,22 @@ class FusionModule:
         self.ret_text_dim = ret_text_dim
         self.k = k
 
+        self.alpha = nn.Parameter(torch.tensor(1.0))
+        self.beta = nn.Parameter(torch.tensor(1.0))
+        self.gamma = nn.Parameter(torch.tensor(1.0))
+
     def __call__(self, query_vec: torch.Tensor, retrieved_vecs: torch.Tensor, projected_vec: torch.Tensor) -> torch.Tensor:
         assert query_vec.shape[1] == self.query_dim
         assert retrieved_vecs.shape[1:] == (self.k, self.ret_text_dim)
         assert projected_vec.shape[1] == self.ret_text_dim
 
         retrieved_vecs = retrieved_vecs.reshape(-1, self.k*self.ret_text_dim)
-        fused_vec = torch.cat((query_vec, retrieved_vecs, projected_vec), dim=-1)
+
+        scaled_query_vec = self.alpha*query_vec
+        scaled_retrieved_vecs = self.beta*retrieved_vecs
+        scaled_projected_vec = self.gamma*projected_vec
+
+        fused_vec = torch.cat((scaled_query_vec, scaled_retrieved_vecs, scaled_projected_vec), dim=-1)
         return fused_vec
     
 if __name__ == '__main__':
